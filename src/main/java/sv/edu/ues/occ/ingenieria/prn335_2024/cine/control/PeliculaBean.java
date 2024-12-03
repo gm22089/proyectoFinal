46913161ec67ed77ddd7d55e908145bd2bc04a09
Natán García <gh20023@ues.edu.sv>
2024-12-03 15:57:54 -0600

    Correciones Pelicula.

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/PeliculaBean.java
@@ -0,0 +1,26 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.control;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Pelicula;
+
+import java.io.Serializable;
+
+@Stateless
+@LocalBean
+public class PeliculaBean extends AbstractDataPersistence<Pelicula> implements Serializable {
+
+    @PersistenceContext(unitName = "CinePU")
+    EntityManager em;
+
+    public PeliculaBean() {
+        super(Pelicula.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/entity/PeliculaCaracteristica.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/entity/PeliculaCaracteristica.java
@@ -1,11 +1,18 @@
 package sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity;
 
 import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "pelicula_caracteristica", schema = "public")
+@XmlRootElement
+@NamedQueries({
+        @NamedQuery(name = "PeliculaCaracteristica.findAll", query = "SELECT p FROM TipoPelicula p"),
+        @NamedQuery(name = "PeliculaCaracteristica.findByIdPelicula", query = "SELECT pc FROM PeliculaCaracteristica pc WHERE pc.idPelicula.idPelicula = :idPelicula order by pc.idPeliculaCaracteristica asc "),
+        @NamedQuery(name = "PeliculaCaracteristica.countByIdPelicula", query = "SELECT COUNT (pc.idPeliculaCaracteristica) FROM PeliculaCaracteristica pc WHERE pc.idPelicula.idPelicula = :idPelicula "),
+        @NamedQuery(name = "PeliculaCaracteristica.findByValor", query = "SELECT p FROM PeliculaCaracteristica p WHERE p.valor = :valor")})
 public class PeliculaCaracteristica implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -58,6 +65,31 @@ public class PeliculaCaracteristica implements Serializable {
 
     public void setValor(String valor) {
         this.valor = valor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPeliculaCaracteristica != null ? idPeliculaCaracteristica.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PeliculaCaracteristica)) {
+            return false;
+        }
+        PeliculaCaracteristica other = (PeliculaCaracteristica) object;
+        if ((this.idPeliculaCaracteristica == null && other.idPeliculaCaracteristica != null) || (this.idPeliculaCaracteristica != null && !this.idPeliculaCaracteristica.equals(other.idPeliculaCaracteristica))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.PeliculaCaracteristica[ idPeliculaCaracteristica=" + idPeliculaCaracteristica + " ]";
     }
 
 }
